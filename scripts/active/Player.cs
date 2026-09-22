cc603450efa99fd8561cbb9ebd5d940c65442ac8
Robert2B9 <57448606+Robert2B9@users.noreply.github.com>
2026-09-22 15:44:06 +0200

    Update Player.cs

--- scripts/active/Player.cs
+++ scripts/active/Player.cs
@@ -12,20 +12,20 @@ namespace game.scripts.active
 		{
 			if (@event.IsActionPressed("move_forward")) _Move(Vector3.Forward);
 			else if (@event.IsActionPressed("move_backward")) _Move(Vector3.Back);
-			else if (@event.IsActionPressed("move_left")) _Move(Vector3.Left);
-			else if (@event.IsActionPressed("move_right")) _Move(Vector3.Right);
+			else if (@event.IsActionPressed("strafe_left")) _Move(Vector3.Left);
+			else if (@event.IsActionPressed("strafe_right")) _Move(Vector3.Right);
 			else if (@event.IsActionPressed("turn_left")) _Turn(-TurnDegrees);
 			else if (@event.IsActionPressed("turn_right")) _Turn(TurnDegrees);
 		}
 
 		private void _Move(Vector3 localDirection)
 		{
-			
+			SetGlobalPosition(GetGlobalPosition() + localDirection * StepDistance);
 		}
 
 		private void _Turn(float degrees)
 		{
-			
+			RotateX(degrees);
 		}
 	}
 }
